946c9b10f6af54762a441c83e7a1c96e5c81b21d
Daniil Baturyn <notfeardan@gmail.com>
2024-11-06 23:18:35 -0600

    Updated script.js

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
-var button = document.querySelector("button");
+const css = document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.getElementById("gradient");
+const button = document.querySelector("button");
 
-function addGradient() {
-  body.style.background =
-    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-  css.textContent = body.style.background + ";";
-}
+const addGradient = () => {
+  body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+  css.textContent = `${body.style.background};`;
+};
 
-function getRandomColor() {
-  return (
-    "#" +
-    Math.floor(Math.random() * 16777215)
-      .toString(16)
-      .padStart(6, "0")
-  );
-}
+const getRandomColor = () =>
+  `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, "0")}`;
 
-function setRandomColors() {
+const setRandomColors = () => {
   color1.value = getRandomColor();
   color2.value = getRandomColor();
   addGradient();
-}
+};
 
 color1.addEventListener("input", addGradient);
 color2.addEventListener("input", addGradient);
